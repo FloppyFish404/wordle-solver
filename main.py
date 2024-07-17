5a50b773a991cf31aa6b2be4d16d43a5363db61f
Charlie Griffith <charlieg4004@gmail.com>
2024-07-17 18:51:56 +1000

    main implemented

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import lib
+import random
 
 
 def main():
-    words = lib.get_local_words_list()
+    guess_pool = lib.get_local_words_list()
+    answer_pool = lib.get_local_words_list()
+    ANSWER = random.choice(answer_pool)
+    print(f'ANSWER IS {ANSWER}')
+
+    turn = 0
+    solved = False
+    feedback = lib.Feedback()
+    guesses = []
+    while not solved and turn <= 6:
+        guess = lib.find_best_guess(guess_pool, answer_pool, feedback, guesses)
+        guesses.append(guess)
+        if guess == ANSWER:
+            solved = True
+        else:
+            new_feedback = lib.get_guess_feedback(guess, ANSWER)
+            feedback.merge(new_feedback)
+            answer_pool = lib.get_possible_answers(feedback, answer_pool)
+        turn += 1
+    lib.show_result(guesses, turn, ANSWER)
 
 
 if __name__ == '__main__':
